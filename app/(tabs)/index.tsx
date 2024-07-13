df6a6c4e575810ac0f1ace57aaadf84addff159f
Maheesha <maheeshafernando18@gmail.com>
2024-07-13 21:30:02 +0530

    update index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,57 +1,74 @@
-import { Image, StyleSheet, Platform, Text, ImageBackground, View, ScrollView } from 'react-native';
+import { Image, StyleSheet, Platform, Text, ImageBackground, View, ScrollView, Button } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { Video } from 'expo-av';
+import { router } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <>
       <View style={styles.logoContainer}>
         <ImageBackground
-          source={require('@/assets/img/zara-logo.png')} 
+          source={require('@/assets/img/zara-logo.png')}
           style={styles.logo}
         >
         </ImageBackground>
       </View>
 
-      <ScrollView style={styles.scrollView} 
-      showsVerticalScrollIndicator={false}>
+      <ScrollView style={styles.scrollView}
+        showsVerticalScrollIndicator={false}>
         <View style={styles.container}>
           <ImageBackground
-            source={require('@/assets/img/home01.jpg')} 
+            source={require('@/assets/img/home01.jpg')}
             style={styles.image}
           >
           </ImageBackground>
 
           <ImageBackground
-            source={require('@/assets/img/home02.jpg')} 
+            source={require('@/assets/img/home02.jpg')}
             style={styles.image}
           >
           </ImageBackground>
 
           <ImageBackground
-            source={require('@/assets/img/home03.jpg')} 
+            source={require('@/assets/img/home03.jpg')}
             style={styles.image}
           >
           </ImageBackground>
           <ImageBackground
-            source={require('@/assets/img/home04.jpg')} 
+            source={require('@/assets/img/home04.jpg')}
             style={styles.image}
           >
           </ImageBackground>
           <ImageBackground
-            source={require('@/assets/img/home05.jpg')} 
+            source={require('@/assets/img/home05.jpg')}
             style={styles.image}
           >
           </ImageBackground>
           <ImageBackground
-            source={require('@/assets/img/home06.jpg')} 
+            source={require('@/assets/img/home06.jpg')}
             style={styles.image}
+
           >
           </ImageBackground>
+
+
         </View>
       </ScrollView>
+      <View>
+        <Button
+          onPress={() => { router.push('(tabs)/search')}}
+          title="Search"
+          color="#000000"
+        />
+
+
+
+
+
+      </View>
     </>
   );
 }
@@ -59,27 +76,31 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start', 
+    justifyContent: 'flex-start',
   },
   image: {
     height: 600,
-    width: '100%', 
+    width: '100%',
     marginBottom: 5,
   },
   scrollView: {
-   
+
   },
   logoContainer: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    zIndex: 1, 
+    zIndex: 1,
   },
   logo: {
     width: '100%',
-    height: 200,
+    height: 180,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 20,
+
   },
+
 });
+
